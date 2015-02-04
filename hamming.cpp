e530bdc850e1afc2107e4a22b4bef4e25e4c1ff4
Eisha Nathan <eisha.nathan@gmail.com>
2015-02-03 19:32:49 -0500

    hamming

--- hamming.cpp
+++ hamming.cpp
@@ -3,6 +3,16 @@
 
 unsigned int hamming(uint64_t x, uint64_t y)
 {
-    // TODO: calculate the hamming distance and return it
-    return 0;
+    uint64_t hamming_dist = 0;
+    uint64_t temp = x^y;
+
+    while (temp!=0) {
+    	temp &= (temp-1);
+    	hamming_dist++;
+    }
+
+
+    return hamming_dist;
+
+    //return 0;
 }
